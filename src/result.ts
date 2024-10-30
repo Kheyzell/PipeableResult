2be188b6b7eea999a9aff8dbff0ba0b3e05ae157
Jeremie Hartz <jeremie.hartz@viseo.com>
2024-10-30 21:10:51 +0100

    feat: pipe

--- src/result.ts
+++ src/result.ts
@@ -1,21 +1,39 @@
-export type Result<Value, Err extends ResultError> = Success<Value> | Failure<Err>;
+export class Result<Value, Err extends ResultError> {
+    private _value: Value;
+    private _err: Err;
 
-type Pipeable<Value, Err extends ResultError> = {
+    private _isFailure = false;
+
+    private constructor(v?: Value, e?: Err) {
+        if (v)
+            this._value = v;
+
+        if (e) {
+            this._err = e;
+            this._isFailure = true;
+        }
+    }
+
+    static succeed<Err extends ResultError>(): Result<any, Err>;
+    static succeed<Value, Err extends ResultError>(v: Value): Result<Value, Err>;
+    static succeed<Value, Err extends ResultError>(v?: Value): Result<Value, Err> {
+        return new Result(v);
+    }
+
+    static fail<Value, Err extends ResultError>(e: Err): Result<Value, Err> {
+        return new Result(null! as Value, e);
+    }
+
     /**
-     * Chains multiple operations that take a `Result` and return a new `Result`. The `pipe`
-     * method allows transformations to be applied sequentially on the `Result`.
-     * 
-     * @example
-     * const result = succeed('hello');
-     * result.pipe(
-     *     map(x => x.toUpperCase()),
-     *     catchErr(e => console.log(e))
-     * );
+     * Returns `true` if the `Result` is a `Success`, `false` otherwise.
      */
-    pipe: (...fns: Array<(result: Result<Value, Err>) => Result<Value, Err>>) => Result<Value, Err>;
-};
+    isSuccess = (): boolean => !this._isFailure;
 
-type ResultInspectable<Value, Err extends ResultError> = {
+    /**
+     * Returns `true` if the `Result` is a `Failure`, `false` otherwise.
+     */
+    isFailure = (): boolean => this._isFailure;
+
     /**
      * Safely get the value inside the Result by handling the error case.
      * @example
@@ -30,47 +48,77 @@ type ResultInspectable<Value, Err extends ResultError> = {
      *     }
      * });
      */
-    unwrap: (obj: {
-        err: (error: Err) => Value
-    }) => Value
+    unwrap(obj: { err: (error: Err) => Value }): Value {
+        if (this._isFailure) {
+            return obj.err(this._err);
+        }
+
+        return this._value;
+    };
 
     /**
      * Returns a string representation of the `Result`. If the `Result` is a `Failure`,
      * the string representation is `Failure(<error>)`. If the `Result` is a `Success`,
      * the string representation is `Success(<value>)`.
      */
-    inspect: () => string;
+    inspect(): string {
+        if (this._isFailure) {
+            return `Failure(${this._err.message})`;
+        }
 
-    /**
-     * Returns `true` if the `Result` is a `Success`, `false` otherwise.
-     */
-    isSuccess: () => boolean;
-
-    /**
-     * Returns `true` if the `Result` is a `Failure`, `false` otherwise.
-     */
-    isFailure: () => boolean;
+        return `Success(${this._value})`;
+    }
 
     /**
      * /!\ Unsafely /!\ Returns the value inside the `Result`.
      * Throws an error if it's a `Failure`.
      */
-    value: () => Value;
+    value(): Value {
+        if (this._isFailure) {
+            throw new Error('Cannot get value from Failure');
+        }
+
+        return this._value;
+    }
 
     /**
      * Returns the error if it's a `Failure`, or `null` if it's a `Success`.
      */
-    error: () => Err | null;
-};
+    error(): Err | null {
+        return this._err;
+    }
 
-type Success<Value> = Pipeable<Value, ResultError> & ResultInspectable<Value, ResultError> & {
-    type: 'success';
-};
-
-type Failure<Err extends ResultError> = Pipeable<any, Err> & ResultInspectable<any, Err> & {
-    type: 'failure';
-    _error: Err;
-};
+    /**
+     * Chains multiple operations that take a `Result` and return a new `Result`. The `pipe`
+     * method allows transformations to be applied sequentially on the `Result`.
+     * 
+     * @example
+     * const result = succeed('hello');
+     * result.pipe(
+     *     map(x => x.toUpperCase()),
+     *     catchErr(e => console.log(e))
+     * );
+     */
+    pipe(): Result<Value, ResultError>;
+    pipe<A>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>): Result<A, ResultError>;
+    pipe<A, B>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>): Result<B, ResultError>;
+    pipe<A, B, C>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>): Result<C, ResultError>;
+    pipe<A, B, C, D>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>): Result<D, ResultError>;
+    pipe<A, B, C, D, E>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>): Result<E, ResultError>;
+    pipe<A, B, C, D, E, F>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>, op6: ResultOperatorFunction<E, ResultError, F, ResultError>): Result<F, ResultError>;
+    pipe<A, B, C, D, E, F, G>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>, op6: ResultOperatorFunction<E, ResultError, F, ResultError>, op7: ResultOperatorFunction<F, ResultError, G, ResultError>): Result<G, ResultError>;
+    pipe<A, B, C, D, E, F, G, H>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>, op6: ResultOperatorFunction<E, ResultError, F, ResultError>, op7: ResultOperatorFunction<F, ResultError, G, ResultError>, op8: ResultOperatorFunction<G, ResultError, H, ResultError>): Result<H, ResultError>;
+    pipe<A, B, C, D, E, F, G, H, I>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>, op6: ResultOperatorFunction<E, ResultError, F, ResultError>, op7: ResultOperatorFunction<F, ResultError, G, ResultError>, op8: ResultOperatorFunction<G, ResultError, H, ResultError>, op9: ResultOperatorFunction<H, ResultError, I, ResultError>): Result<I, ResultError>;
+    pipe<A, B, C, D, E, F, G, H, I, J>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>, op6: ResultOperatorFunction<E, ResultError, F, ResultError>, op7: ResultOperatorFunction<F, ResultError, G, ResultError>, op8: ResultOperatorFunction<G, ResultError, H, ResultError>, op9: ResultOperatorFunction<H, ResultError, I, ResultError>, op10: ResultOperatorFunction<I, ResultError, J, ResultError>): Result<J, ResultError>;
+    pipe<A, B, C, D, E, F, G, H, I, J, K>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>, op6: ResultOperatorFunction<E, ResultError, F, ResultError>, op7: ResultOperatorFunction<F, ResultError, G, ResultError>, op8: ResultOperatorFunction<G, ResultError, H, ResultError>, op9: ResultOperatorFunction<H, ResultError, I, ResultError>, op10: ResultOperatorFunction<I, ResultError, J, ResultError>, op11: ResultOperatorFunction<J, ResultError, K, ResultError>): Result<K, ResultError>;
+    pipe<A, B, C, D, E, F, G, H, I, J, K, L>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>, op6: ResultOperatorFunction<E, ResultError, F, ResultError>, op7: ResultOperatorFunction<F, ResultError, G, ResultError>, op8: ResultOperatorFunction<G, ResultError, H, ResultError>, op9: ResultOperatorFunction<H, ResultError, I, ResultError>, op10: ResultOperatorFunction<I, ResultError, J, ResultError>, op11: ResultOperatorFunction<J, ResultError, K, ResultError>, op12: ResultOperatorFunction<K, ResultError, L, ResultError>): Result<L, ResultError>;
+    pipe<A, B, C, D, E, F, G, H, I, J, K, L, M>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>, op6: ResultOperatorFunction<E, ResultError, F, ResultError>, op7: ResultOperatorFunction<F, ResultError, G, ResultError>, op8: ResultOperatorFunction<G, ResultError, H, ResultError>, op9: ResultOperatorFunction<H, ResultError, I, ResultError>, op10: ResultOperatorFunction<I, ResultError, J, ResultError>, op11: ResultOperatorFunction<J, ResultError, K, ResultError>, op12: ResultOperatorFunction<K, ResultError, L, ResultError>, op13: ResultOperatorFunction<L, ResultError, M, ResultError>): Result<M, ResultError>;
+    pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>, op6: ResultOperatorFunction<E, ResultError, F, ResultError>, op7: ResultOperatorFunction<F, ResultError, G, ResultError>, op8: ResultOperatorFunction<G, ResultError, H, ResultError>, op9: ResultOperatorFunction<H, ResultError, I, ResultError>, op10: ResultOperatorFunction<I, ResultError, J, ResultError>, op11: ResultOperatorFunction<J, ResultError, K, ResultError>, op12: ResultOperatorFunction<K, ResultError, L, ResultError>, op13: ResultOperatorFunction<L, ResultError, M, ResultError>, op14: ResultOperatorFunction<M, ResultError, N, ResultError>): Result<N, ResultError>;
+    pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O>(op1: ResultOperatorFunction<Value, ResultError, A, ResultError>, op2: ResultOperatorFunction<A, ResultError, B, ResultError>, op3: ResultOperatorFunction<B, ResultError, C, ResultError>, op4: ResultOperatorFunction<C, ResultError, D, ResultError>, op5: ResultOperatorFunction<D, ResultError, E, ResultError>, op6: ResultOperatorFunction<E, ResultError, F, ResultError>, op7: ResultOperatorFunction<F, ResultError, G, ResultError>, op8: ResultOperatorFunction<G, ResultError, H, ResultError>, op9: ResultOperatorFunction<H, ResultError, I, ResultError>, op10: ResultOperatorFunction<I, ResultError, J, ResultError>, op11: ResultOperatorFunction<J, ResultError, K, ResultError>, op12: ResultOperatorFunction<K, ResultError, L, ResultError>, op13: ResultOperatorFunction<L, ResultError, M, ResultError>, op14: ResultOperatorFunction<M, ResultError, N, ResultError>, op15: ResultOperatorFunction<N, ResultError, O, ResultError>): Result<O, ResultError>;
+    pipe(...fns: Array<ResultOperatorFunction<any, ResultError, any, ResultError>>): Result<any, ResultError> {
+        return pipeFromArray(fns)(this);
+    }
+}
 
 export class ResultError extends Error {
     constructor(name: string, message: string) {
@@ -81,13 +129,12 @@ export class ResultError extends Error {
 }
 
 /** Type Guards **/
-
-const isSuccess = <Value, Err extends ResultError>(result: Result<Value, Err>): result is Success<Value> => {
-    return result.type === 'success';
+const isResult = <Value, Err extends ResultError>(result: any): result is Result<Value, Err> => {
+    return result && 'type' in result && 'pipe' in result;
 };
 
-const isFailure = <Value, Err extends ResultError>(result: Result<Value, Err>): result is Failure<Err> => {
-    return result.type === 'failure';
+const isError = <Err extends ResultError>(error: any): error is Err => {
+    return error instanceof ResultError;
 };
 
 /** Operators **/
@@ -104,10 +151,10 @@ const isFailure = <Value, Err extends ResultError>(result: Result<Value, Err>): 
 export const map = <Value, Err extends ResultError, A>(
     fn: (arg: Value) => A
 ) => (result: Result<Value, Err>): Result<A, Err> => {
-    if (isSuccess(result)) {
-        return succeed(fn(result.value()));
+    if (result.isSuccess()) {
+        return succeed(fn(result.value())) as Result<A, Err>;
     }
-    return result as Result<A, Err>;
+    return Result.fail(result.error()!)
 };
 
 /**
@@ -122,10 +169,10 @@ export const map = <Value, Err extends ResultError, A>(
 export const mapErr = <Value, Err extends ResultError, B extends ResultError>(
     fn: (arg: Err) => B
 ) => (result: Result<Value, Err>): Result<Value, B> => {
-    if (isFailure(result)) {
-        return fail(fn(result._error));
+    if (result.isFailure()) {
+        return fail(fn(result.error()!)) as Result<Value, B>;
     }
-    return result as Result<Value, B>;
+    return Result.succeed(result.value());
 };
 
 /**
@@ -137,12 +184,23 @@ export const mapErr = <Value, Err extends ResultError, B extends ResultError>(
  * const chainedResult = result.pipe(chain(x => succeed(x * 2))); // Returns a `Success` with value 10
  */
 export const chain = <Value, Err extends ResultError, A>(
-    fn: (arg: Value) => Result<A, Err>
+    fn: (arg: Value) => (Result<A, Err> | A)
 ) => (result: Result<Value, Err>): Result<A, Err> => {
-    if (isSuccess(result)) {
-        return fn(result.value());
+    if (result.isSuccess()) {
+        const fnValue = fn(result.value());
+        if (isResult(fnValue)) {
+            return fnValue as Result<A, Err>;
+        }
+
+        if (isError<Err>(fnValue)) {
+            const error = fnValue as Err;
+            return Result.fail(error) as Result<A, Err>;
+        }
+
+        return succeed(fnValue) as Result<A, Err>;
     }
-    return result as Result<A, Err>;
+
+    return Result.fail(result.error()!);
 };
 
 /**
@@ -154,12 +212,22 @@ export const chain = <Value, Err extends ResultError, A>(
  * const handledResult = result.pipe(chainErr(e => succeed([]))); // Converts the failure to success
  */
 export const chainErr = <Value, Err extends ResultError, B extends ResultError>(
-    fn: (arg: Err) => Result<Value, B>
+    fn: (arg: Err) => (Result<Value, B> | B)
 ) => (result: Result<Value, Err>): Result<Value, B> => {
-    if (isFailure(result)) {
-        return fn(result._error);
+    if (result.isFailure()) {
+        const fnValue = fn(result.error()!);
+        if (isResult(fnValue)) {
+            return fnValue as Result<Value, B>;
+        }
+
+        if (isError(fnValue)) {
+            return Result.fail(fnValue) as Result<Value, B>;
+        }
+
+        return succeed(fnValue) as Result<Value, B>;
     }
-    return result as Result<Value, B>;
+
+    return Result.succeed(result.value()) as Result<Value, B>;
 };
 
 /**
@@ -173,7 +241,7 @@ export const chainErr = <Value, Err extends ResultError, B extends ResultError>(
 export const tap = <Value, Err extends ResultError>(
     fn: (arg: Value) => void
 ) => (result: Result<Value, Err>): Result<Value, Err> => {
-    if (isSuccess(result)) {
+    if (result.isSuccess()) {
         fn(result.value());
     }
     return result;
@@ -190,8 +258,8 @@ export const tap = <Value, Err extends ResultError>(
 export const catchErr = <Value, Err extends ResultError>(
     fn: (arg: Err) => void
 ) => (result: Result<Value, Err>): Result<Value, Err> => {
-    if (isFailure(result)) {
-        fn(result._error);
+    if (result.isFailure()) {
+        fn(result.error()!);
     }
     return result;
 };
@@ -210,45 +278,46 @@ export const catchErr = <Value, Err extends ResultError>(
 export const match = <Value, Err extends ResultError>(
     handlers: { ok: (arg: Value) => void; err: (arg: Err) => void }
 ) => (result: Result<Value, Err>): Result<Value, Err> => {
-    if (isSuccess(result)) {
+    if (result.isSuccess()) {
         handlers.ok(result.value());
     } else {
-        handlers.err(result._error);
+        handlers.err(result.error()!);
     }
     return result;
 };
 
 /** Constructors **/
-
-const success = <O>(value: O): Success<O> => ({
-    type: 'success',
-    pipe: (...fns) => fns.reduce((acc, fn) => fn(acc), succeed(value)),
-    unwrap: () => value,
-    inspect: () => `Success(${value})`,
-    isSuccess: () => true,
-    isFailure: () => false,
-    value: () => value,
-    error: () => null
-});
-
-const failure = <E extends ResultError>(error: E): Failure<E> => ({
-    type: 'failure',
-    _error: error,
-    pipe: (...fns) => fns.reduce((acc, fn) => fn(acc), fail(error)),
-    unwrap: obj => obj.err(error),
-    inspect: () => `Failure(${error.message})`,
-    isSuccess: () => false,
-    isFailure: () => true,
-    value: () => { throw new Error('Cannot get value from Failure'); },
-    error: () => error
-});
-
 export function succeed(): Result<any, any>;
 export function succeed<O>(arg: O): Result<O, any>;
 export function succeed<O>(arg?: O): Result<O | undefined, any> {
-    return success(arg);
+    return Result.succeed(arg);
 }
 
 export function fail<E extends ResultError>(arg: E): Result<any, E> {
-    return failure(arg);
+    return Result.fail(arg);
+}
+
+type OperatorFunction<A, B> = (a: A) => B;
+type ResultOperatorFunction<V, E extends ResultError, V2, E2 extends ResultError> = (a: Result<V, E>) => Result<V2, E2>;
+
+export function identity<T>(x: T): T {
+    return x;
+}
+
+export interface UnaryFunction<T, R> {
+    (source: Result<T, ResultError>): Result<R, ResultError>;
+}
+
+export function pipeFromArray<T, R>(fns: Array<UnaryFunction<T, R>>): UnaryFunction<T, R> {
+    if (fns.length === 0) {
+        return identity as UnaryFunction<any, any>;
+    }
+
+    if (fns.length === 1) {
+        return fns[0];
+    }
+
+    return function piped(input: Result<T, ResultError>): Result<R, ResultError> {
+        return fns.reduce((prev: any, fn: UnaryFunction<T, R>) => fn(prev), input as any);
+    };
 }
